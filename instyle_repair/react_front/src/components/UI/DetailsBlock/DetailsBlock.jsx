158cb93f564e7347963088b1e89a6057ad5cd011
HrynykNazar <hrynyknazar@gmail.com>
2023-05-02 02:03:51 +0300

    change modal window

--- instyle_repair/react_front/src/components/UI/DetailsBlock/DetailsBlock.jsx
+++ instyle_repair/react_front/src/components/UI/DetailsBlock/DetailsBlock.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import cl from "./DetailsBlock.module.css"
 
 
 const DetailsBlock = function ({children, mutable, detail}) {
+
+    const [itemValue, setItemValue] = useState(detail)
+
+    function changeItem() {
+        console.log(itemValue)
+    }
+
     return (
         <div className={cl.detail}>
             <h1>{children}</h1>
             {mutable
             ? 
-            <div>
-                <input className={cl.detailinput} placeholder={detail}/>
-                <div className={cl.refresh}></div>
-            </div> 
-            : <div className={cl.detailinput}>{detail}</div>
+                <div>
+                    <input 
+                        className={cl.detailinput} 
+                        placeholder={itemValue}
+                        onChange={ (e) => setItemValue(e.target.value)}
+                    />
+                    <div 
+                        className={cl.refresh}
+                        onClick={changeItem}
+                    >
+                    </div>
+                </div> 
+                :
+                <div>
+                    <div className={cl.detail_input_block}>
+                        {detail}
+                    </div>
+
+                </div>
+
             }
         </div>
 
--- instyle_repair/react_front/src/components/UI/RepairModal/RepairModal.jsx
+++ instyle_repair/react_front/src/components/UI/RepairModal/RepairModal.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import './RepairModal.css';
 import DetailsBlock from "../DetailsBlock/DetailsBlock";
+import moment from 'moment';
 
 
 const RepairModal = function ({visible, setVisible, repair}) {
+
+    const [answerInput, setAnswerInput] = useState('')
     
     const rootClasses = ['RepairModal']
 
@@ -17,43 +20,52 @@ const RepairModal = function ({visible, setVisible, repair}) {
 
                 <div className="popUp__header header-popUp">
                     <div className="header-popUp__worrantyDate">
-                        <div className="header-popUp__worranty">Гарантійний</div>
-                        <div className="header-popUp__date">21.03.2021</div>
+                        <div className="header-popUp__worranty">{repair.warranty ? 'Гарантійний' : ''}</div>
+                        <div className="header-popUp__date"><strong>Проданий</strong>: 21.03.2021</div>
                     </div>
-                    <div className="header-popUp__number">№93799</div>
-                    <div className="header-popUp__shop">InStyle </div>
+                    <div className="header-popUp__number"><strong>Ремонт</strong>: №{repair.number}</div>
+                    <div>
+                        <div className="header-popUp__shop"><strong>Магазин</strong>: {repair.shop}</div>
+                        <div className="header-popUp__date-create"><strong>Прийнятий</strong>: {moment(repair.time_create.time_work).format('D.MM.YYYY')}</div>
+                    </div>
                 </div>
                 <div className="popUp__body body-popUp">
 
                     <div>
                         <DetailsBlock mutable={true} detail={repair.imei}>IMEI</DetailsBlock>
-                        <DetailsBlock mutable={true} detail={repair.model}>Модель</DetailsBlock>
-                        <DetailsBlock mutable={true}>Пароль</DetailsBlock>
+                        <DetailsBlock mutable={false} detail={repair.model}>Модель</DetailsBlock>
+                        <DetailsBlock mutable={true} detail={repair.password}>Пароль</DetailsBlock>
                     </div>
                     <div>
-                        <DetailsBlock mutable={true}>Клієнт</DetailsBlock>
-                        <DetailsBlock mutable={true}>Номер телефону</DetailsBlock>
-                        <DetailsBlock mutable={true}>Час завершення</DetailsBlock>
+                        <DetailsBlock mutable={false}>Клієнт</DetailsBlock>
+                        <DetailsBlock mutable={false}>Постачальник</DetailsBlock>
+                        <DetailsBlock 
+                            mutable={true}
+                            detail={`${moment(repair.time_create.time_create).format('HH:mm')} - ${moment(repair.time_create.time_work).format('HH:mm')}`}
+                        >
+                            Час завершення
+                        </DetailsBlock>
                     </div>
                     <div>
                         <DetailsBlock mutable={false} detail={repair.status}>Статус</DetailsBlock>
-                        <DetailsBlock mutable={false}>Майстер</DetailsBlock> 
-                        <DetailsBlock mutable={false}>Менеджер</DetailsBlock>
+                        <DetailsBlock mutable={false} detail={`${repair.master ? repair.master.name : 'Не прийнятий'}`}>Майстер</DetailsBlock> 
+                        <DetailsBlock mutable={false} detail={'Поки що нема'}>Менеджер</DetailsBlock>
                     </div>
 
                 </div>
                 <div className="popUp__malfunction malfunction-popUp">
                     <div className="malfunction-popUp__title">Несправність</div>
-                    <div className="malfunction-popUp__text">Кусок сраного гімна ніхуя не включається і їбе мені мозок. Ще блять ніц не тримав батарейку, лиш встигав секунди парнуху глипну і пездонс. Зробіт  бо віддав  грошуй а воно не їбашит ніхуя! Повідомив клієнт!</div>
+                    <div className="malfunction-popUp__text">{repair.defect}</div>
                 </div>
                 <div className="popUp__notes notes-popUp">
                     <div className="notes-popUp__title">Відписати за ремонт</div>
-                    <div className="notes-popUp__text">
-                        Ви вхуйкали свій тріпак до такої степені шо тепе це ріщє зможе лиш стіл підпирати. Можете забрати його собі і запхати собі в окуляр, кончений вилупок. Треба  з телефоном поводитись а нє як оце от...
-                    </div>
+                    <input
+                        className="notes-popUp__text"
+                        onChange={ (e) => setAnswerInput(e.target.value)}
+                    />
                 </div>
                 <div className="popUp__actions action-popUp">
-                    <a href="#" className="action-popUp__close">Закрити</a>
+                    <a href="#" className="action-popUp__close">Закрити ремонт</a>
                     <a href="#" className="action-popUp__history">Історія</a>
                     <a href="#" className="action-popUp__answer">Відповісти</a>
                 </div>
