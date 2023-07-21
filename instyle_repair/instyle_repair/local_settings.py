6e4e02c670b8715bd22a051061bc21c2607f31d8
HrynykNazar <hrynyknazar@gmail.com>
2023-07-22 00:50:25 +0300

    prepare to deploy

---
+++ instyle_repair/instyle_repair/local_settings.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = 'django-insecure-mqmgw=v1e9s0jk+!rx6&-s$zo1%fng0@qlni@m1dtynp(!pum8'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'instyle',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "service/build/static")
+]
