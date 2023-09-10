3468273df3376b98921aab44c27727d95cab9bfd
HrynykNazar <hrynyknazar@gmail.com>
2023-09-11 00:22:03 +0300

    add one point to api

--- bot_parser/prod_config.py
+++ bot_parser/prod_config.py
@@ -1,4 +1,4 @@
-token = '6076395112:AAEdPr_UP2lHMNfXHwSclPqNFPnaR1iIkSs'
+token = '6166447970:AAGXtRoaNVgE_oy2NiyATPYEkQvD1qLCmo4'  # InstyleServiceBot
 url = 'http://5.45.87.133:8000/service/api/v1/addrep/'
-my_user_id = 375385945
-bot_id = 375385945
+my_user_id = 375385945  # тут так і лишаєьтся  -  щоб мені приходили репорти
+bot_id = 375385945  # тут ID бота який в групі
---
+++ react_front/src/API/BotMessage.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default class SendMessageBot {
+    static async sendMessage(number, price) {
+        const response = await axios.post('https://api.telegram.org/bot6076395112:AAEdPr_UP2lHMNfXHwSclPqNFPnaR1iIkSs/sendMessage', {
+        'chat_id': 375385945,
+        'text': `${number}***${price}`
+        })
+        return response
+    }
+
+}
