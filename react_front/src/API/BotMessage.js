c7ab221322760090f8076063ac90d15c8dd58f30
HrynykNazar <hrynyknazar@gmail.com>
2023-09-13 01:33:30 +0300

    i forgot about bot token

--- react_front/src/API/BotMessage.js
+++ react_front/src/API/BotMessage.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class SendMessageBot {
     static async sendMessage(number, price) {
-        const response = await axios.post('https://api.telegram.org/bot6076395112:AAEdPr_UP2lHMNfXHwSclPqNFPnaR1iIkSs/sendMessage', {
+        const response = await axios.post('https://api.telegram.org/bot6166447970:AAGXtRoaNVgE_oy2NiyATPYEkQvD1qLCmo4/sendMessage', {
         'chat_id': -1001355858016,
         'text': `${number}***${price}`
         })
